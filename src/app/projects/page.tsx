3b58d66094ba70e37d75418adfb69f1e3b3b0672
Cameron Salazar <cameronsalazar.1102@gmail.com>
2025-09-16 17:00:50 -0500

    Starting code for Project tab
    Basic layout for project tab

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,17 @@
+export default function Projects() {
+    return (
+      <section className="max-w-3xl mx-auto p-8">
+        <h2 className="text-3xl font-semibold mb-6">Projects</h2>
+        <ul className="space-y-4">
+          <li className="rounded-xl border p-4">
+            <h3 className="font-medium">Arioso</h3>
+            <p className="text-sm text-gray-600">Social media web application for music lovers</p>
+
+            <h4 className="font-medium">Vision Chess</h4>
+            <p className="text-sm text-gray-600">Web based application to play chess for the visually impaired</p>
+          </li>
+        </ul>
+      </section>
+    );
+  }
+  
